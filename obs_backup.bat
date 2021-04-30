a0ff91f4cde8b641cf1e0ea89835b95b3e4fbcf8
Konstantin Lübeck <konze@konze.org>
2021-04-30 20:18:49 +0200

    Added deletion of older backups.

--- obs_backup.bat
+++ obs_backup.bat
@@ -7,18 +7,24 @@
 :: TikTok: https://tiktok.com/@k0nze.gg
 :: Discord: https://discord.k0nze.gg
 
-:: path to backup directory
+:: set backup directory path
 SET "BACKUP_DIR_PATH=%UserProfile%\Documents\obs_backup"
+
+:: set amount of days after a backup should be deleted
+SET "DELETE_BACKUP_AFTER_DAYS=7"
 
 :: path to OBS settings
 SET "OBS_SETTINGS_PATH=%AppData%\obs-studio"
 
 :: add date to backup directory
 SET DATE_TIME_STRING=%date:~-4%_%date:~3,2%_%date:~0,2%-%time:~0,2%_%time:~3,2%_%time:~6,2%
-SET "BACKUP_DIR_PATH=%BACKUP_DIR_PATH%\%DATE_TIME_STRING%_obs_backup"
+SET "BACKUP_DIR_PATH_DATE=%BACKUP_DIR_PATH%\%DATE_TIME_STRING%_obs_backup"
 
 :: make a backup directory
-MD %BACKUP_DIR_PATH%
+MD "%BACKUP_DIR_PATH_DATE%"
 
 :: copy files 
-xcopy %OBS_SETTINGS_PATH% %BACKUP_DIR_PATH% /E/H/I
+xcopy "%OBS_SETTINGS_PATH%" "%BACKUP_DIR_PATH_DATE%" /E/H/I
+
+:: delete old backups
+forfiles -p "%BACKUP_DIR_PATH%" -s -m *.* -d %DELETE_BACKUP_AFTER_DAYS% -c "cmd /c del @path"
